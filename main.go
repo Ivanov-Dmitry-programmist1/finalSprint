2e8d46e0a8ed86602f8f74175c90b5625d440c1e
Ivanov-Dmitry-programmist1 <d89654216069@gmail.com>
2025-06-28 12:24:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,41 +1,50 @@
 package main
 
+package main
+
 import (
-	"fmt"
-	"go_final_project/go_final_project/pkg/db"
+	"database/sql"
 	"log"
+	"net/http"
 	"os"
 
-	"go1f/pkg/server"
-)
-func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf(".env файл не найден")
-	}
-	port := os.Getenv("TODO_PORT")
+	"github.com/LTVgreater5CPi/go_final_project/tasks_service"
 
-	DB, err := database.InitDB()
-	if err != nil {
-		panic(err)
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7540"
 	}
-	defer DB.Close()
+
+	appPassword = os.Getenv("TODO_PASSWORD")
+	if appPassword == "" {
+		log.Println("Переменная TODO_PASSWORD не установлена. Аутентификация отключена.")
+	}
+
+	db, err := setupDB()
+	if err != nil {
+		log.Fatalf("Ошибка настройки БД: %v", err)
+	}
+	defer db.Close()
 
 	webDir := "./web"
 	fileServer := http.FileServer(http.Dir(webDir))
 	http.Handle("/", fileServer)
 
-	http.HandleFunc("/api/nextdate", handlers.NextDateHandler)
-	http.HandleFunc("/api/task", tools.AuthMiddleware(handlers.TaskActionHandler))
-	http.HandleFunc("/api/tasks", tools.AuthMiddleware(handlers.GetAllTasks))
-	http.HandleFunc("/api/task/done", tools.AuthMiddleware(handlers.TaskDone))
-	http.HandleFunc("/api/signin", handlers.SignInHandler)
+	http.HandleFunc("/api/nextdate", tasks_service.NextDateHandler)
+	http.HandleFunc("/api/task", authMidW(tasks_service.MakeHandler(taskHandler, db)))
+	http.HandleFunc("/api/tasks", authMidW(tasks_service.MakeHandler(tasks_service.TasksHandler, db)))
+	http.HandleFunc("/api/task/done", authMidW(tasks_service.MakeHandler(tasks_service.TaskDoneHandler, db)))
+	http.HandleFunc("/api/signin", tasks_service.MakeHandler(signInHandler, db))
 
-	fmt.Println("Сервер запущен на порте:", port)
-	err = http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		panic(err)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
 	}
 }
+
 func taskHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	switch r.Method {
 	case http.MethodPost:
